2547bec534460df5dc4d3742880c2b36b38dc5c6
h1data <77904565+h1data@users.noreply.github.com>
2022-06-20 23:28:55 +0900

    exclude sheet ID from URL and other refactoring

--- scripts/download.js
+++ scripts/download.js
@@ -1,76 +1,109 @@
-// functions to build CSV data
-const buildFunctions =
-  [
-    [buildDictionaryNormal, buildDictionaryWithNumber, buildDictionaryProvisional],
-    [buildGuidesNormal, null, buildGuidesProvisional]
-  ];
+/**
+ * generates download data
+ * @param {number} fileType referred to FileType
+ * @param {boolean} numbered 
+ * @param {boolean} provisional 
+ * @returns {string} contents for download data
+ */ 
+function getCsvData(fileType, numbered, provisional) {
+  console.log('fileType', fileType);
+  console.log('numbered', numbered);
+  console.log('provisional', provisional);
+  
+  // choose function to generate data per line
+  if (fileType == FileType.dictionary) {
+    if (provisional) {
+      var buildFunction = buildDictionaryProvisional;
+    } else {
+      var buildFunction = buildDictionaryNormal;
+    }
+  } else if (fileType == FileType.guides) {
+    if (provisional) {
+      var buildFunction = buildGuidesProvisional;
+    } else {
+      var buildFunction = buildGuidesNormal;
+    }
+  } else {
+    throw 'invalid file type: ' + fileType;
+  }
 
-// generates download data
-function getCsvData(id, type, mode) {
-  // the input sheet name to be referenced
-  const sheetName = sheetNames[type];
+  let sheetName = fileType == FileType.dictionary ? 'dictionarySheet' : 'guidesSheet';
+  const spreadsheet = SpreadsheetApp.openById(PropertiesService.getScriptProperties().getProperty('sheetID'))
+                                    .getSheetByName(PropertiesService.getScriptProperties().getProperty(sheetName));
   
-  // define function to generate data per line
-  const buildFunction = buildFunctions[type][mode];
-  const spreadsheet = validateAndGetSpreadsheet(id);
-  
-  var inputArray = spreadsheet.getSheetByName(sheetName).getDataRange().getValues();
+  var inputArray = spreadsheet.getDataRange().getValues();
   var outputArray = [];
   // line 1 is the header thus loop begins from line 2 (i=0)
   for (let i = 1; i < inputArray.length; i++) {
     buildFunction(inputArray[i], outputArray);
   }
   return outputArray.join('\r\n').concat('\r\n');
+
+  function buildDictionaryNormal(inputArray, outputArray) {
+    if (inputArray[4] == '') {
+      outputArray.push(inputArray[1].concat(',', treatQuot(inputArray[2]), ',', treatQuot(inputArray[3].concat(numbered ? inputArray[0] : ''))));
+    } else {
+      outputArray.push(inputArray[1].concat(',', treatQuot(inputArray[2]), ',', treatQuot(inputArray[4].concat(numbered ? inputArray[0] : ''))));
+    }
+  }
+  
+  function buildDictionaryProvisional(inputArray, outputArray) {
+    if (inputArray[3] == '') {
+      // TODO 『』有りを提出したらreplaceメソッドを削る
+      outputArray.push(inputArray[1].concat(',', treatQuot(inputArray[2]), ',', treatQuot(inputArray[4].concat(numbered ? inputArray[0] : '').replace(/[『,』]/g,''))));
+    } else {
+      outputArray.push(inputArray[1].concat(',', treatQuot(inputArray[2]), ',', treatQuot(inputArray[3].concat(numbered ? inputArray[0] : ''))));
+    }
+  }
+  
+  function buildGuidesNormal(inputArray, outputArray) {
+    if (inputArray[0] == '' || inputArray[0] == '/'){
+      outputArray.push(inputArray[0]);
+    } else if (inputArray[2] == '') {
+      outputArray.push(inputArray[0].concat('; ', inputArray[1]));
+    } else {
+      outputArray.push(inputArray[0].concat('; ', inputArray[2]));
+    }
+  }
+            
+  function buildGuidesProvisional(inputArray, outputArray) {
+    if (inputArray[0] == '' || inputArray[0] == '/'){
+      outputArray.push(inputArray[0]);
+    } else if (inputArray[1] == '') {
+      outputArray.push(inputArray[0].concat('; ', inputArray[2]));
+    } else {
+      outputArray.push(inputArray[0].concat('; ', inputArray[1]));
+    }
+  }
+            
+  /**
+   * replace '"' into '""' and enclose str in '""' if it includes ',' or '"'.
+   * @param {string} str input string
+   * @return {string} treated string
+   */
+  function treatQuot(str) {
+    str = str.replace(/"/g,'""');  // instead of str.replaceAll('"', '""');
+    if (str.includes(',') || str.includes('"')) str = '"'.concat(str, '"');
+    return str;
+  }
+  
 }
 
-function buildDictionaryNormal(inputArray, outputArray) {
-  if (inputArray[4] == '') {
-    outputArray.push(inputArray[1].concat(',', treatQuot(inputArray[2]), ',', treatQuot(inputArray[3])));
+/**
+ * Get file name for download
+ * @param {number} fileType referred to FileType map
+ * @param {boolean} numbered 
+ * @param {boolean} provisional 
+ */
+function getFileName(fileType, numbered, provisional) {
+  if (fileType == FileType.dictionary) {
+    var fileName = dictionaryFileName;
+  } else if (fileType == FileType.guides) {
+    var fileName = guidesFileName;
   } else {
-    outputArray.push(inputArray[1].concat(',', treatQuot(inputArray[2]), ',', treatQuot(inputArray[4])));
+    throw 'invalid file type: ' + fileType;
   }
-}
-
-function buildDictionaryWithNumber(inputArray, outputArray) {
-  if (inputArray[4] == '') {
-    outputArray.push(inputArray[1].concat(',', treatQuot(inputArray[2]), ',', treatQuot(inputArray[3].concat(inputArray[0]))));
-  } else {
-    outputArray.push(inputArray[1].concat(',', treatQuot(inputArray[2]), ',', treatQuot(inputArray[4].concat(inputArray[0]))));
-  }
-}
-
-function buildDictionaryProvisional(inputArray, outputArray) {
-  if (inputArray[3] == '') {
-    // TODO 『』有りを提出したらreplaceメソッドを削る
-    outputArray.push(inputArray[1].concat(',', treatQuot(inputArray[2]), ',', treatQuot(inputArray[4].replace(/[『,』]/g,''))));
-  } else {
-    outputArray.push(inputArray[1].concat(',', treatQuot(inputArray[2]), ',', treatQuot(inputArray[3])));
-  }
-}
-
-function buildGuidesNormal(inputArray, outputArray) {
-  if (inputArray[0] == '' || inputArray[0] == '/'){
-    outputArray.push(inputArray[0]);
-  } else if (inputArray[2] == '') {
-    outputArray.push(inputArray[0].concat('; ', inputArray[1]));
-  } else {
-    outputArray.push(inputArray[0].concat('; ', inputArray[2]));
-  }
-}
-          
-function buildGuidesProvisional(inputArray, outputArray) {
-  if (inputArray[0] == '' || inputArray[0] == '/'){
-    outputArray.push(inputArray[0]);
-  } else if (inputArray[1] == '') {
-    outputArray.push(inputArray[0].concat('; ', inputArray[2]));
-  } else {
-    outputArray.push(inputArray[0].concat('; ', inputArray[1]));
-  }
-}
-          
-// replace '"' into '""' and enclose str in '""' if it includes ',' or '"'.
-function treatQuot(str) {
-  str = str.replace(/"/g,'""');  // instead of str.replaceAll('"', '""');
-  if (str.includes(',') || str.includes('"')) str = '"'.concat(str, '"');
-  return str;
+  if (provisional) fileName += '_provisional';
+  if (numbered) fileName += '_withnumber';
+  return fileName;
 }
--- scripts/variables.js
+++ scripts/variables.js
@@ -1,3 +1,7 @@
+// file names
+const dictionaryFileName = 'ja.Dictionary';
+const guidesFileName = 'ja.Guides';
+
 // message
 const text = {
   title: 'Pathos 日本語 Translation Helper',
@@ -36,17 +40,3 @@ const text = {
     memoColumn: [11, 8]
   }
 };
-
-// the input sheet names for each type (dictionary, guides)
-const sheetNames = ['入力用', 'ガイド入力用'];
-
-// for validation of if the spreadsheet is proper one
-const spreadsheetName = '[test] Pathos 日本語 translation';
-
-// file names for each type and mode
-const fileNames = [
-  // type=0: Dictionary file
-  ['ja.Dictionary', 'ja.Dictionary_withnumber', 'ja.Dictionary_provisional'],
-  // type=1: Guides files
-  ['ja.Guides', 'error', 'ja.Guides_provisional']
-];
